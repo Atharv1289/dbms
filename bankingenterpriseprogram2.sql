3ab585f9d113b707dad5cf6903864880dc518e03
Atharv1289 <47417342+Atharv1289@users.noreply.github.com>
2020-05-13 15:45:04 +0530

    Update bankingenterpriseprogram2.sql

--- bankingenterpriseprogram2.sql
+++ bankingenterpriseprogram2.sql
@@ -1,5 +1,3 @@
-DROP database BANKING_ENTERPRISE;
-
 create database BANKING_ENTERPRISE;
 use BANKING_ENTERPRISE;
 create table Branch(
@@ -62,7 +60,14 @@ insert into Loan values(
 1,"SBI_Chamrajpet",1000),(2,"SBI_ResidencyRoad",2000),(3,"SBI_ShivajiRoad",3000),(4,"SBI_ParliamentRoad",4000),(5,"SBI_Jantarmantar",5000);
 select * from Loan;
 
+create table borrower(
+customername varchar(30),
+loan_num int,
+foreign key(customername) references  Bank_Customer (customername),
+foreign key(loan_num) references Loan(loan_num));
 
+insert into borrower values('Avinash',1), ('Dinesh',2), ('Nikil',3), ('Avinash', 4), ('Dinesh', 5);
+				      
 select C.customername 
 from Bank_Customer C
 where exists (
@@ -73,12 +78,12 @@ where exists (
                 group by D.customername
                 having count(D.customername) >=2);
                 
-select BC.customername from Bank_Customer BC
-where not exists(
-					select branch_name from Branch branch_city = 'Delhi'
-                    except
-                    select BA.branch_name from Depositor D , Bank_account BA
-                    where D.acc_no = BA.acc_no AND BC.customername = D.customername);
+select BC.customer_name from BankCustomer BC where not exists(
+	select branch_name from Branch where branch_city = 'Delhi'
+	and branch_name not in(
+    select BA.branch_name from Depositer D, BankAccount BA
+	where D.accno = BA.accno and BC.customer_name = D.customer_name)
+);
                     
 delete from Bank_account 
 where branch_name in (select branch_name 
